6cc9389fb9efe434f168e5341738a3fddfeda6cb
Colin Snover <colin@alpha.zetafleet.com>
2010-09-10 16:56:18 -0500

    Fixes incorrect rebinding of load event & passes an event object when calling the newly-added handler.

--- ahpi.imgload.js
+++ ahpi.imgload.js
@@ -11,10 +11,10 @@
  * $(images).bind('load', function (e) {
  *   // Do stuff on load
  * });
- * 
+ *
  * Note that you can bind the 'error' event on data uri images, this will trigger when
  * data uri images isn't supported.
- * 
+ *
  * Tested in:
  * FF 3+
  * IE 6-8
@@ -23,21 +23,22 @@
  */
 (function ($) {
 	$.event.special.load = {
-		add: function (hollaback) {
-			if ( this.nodeType === 1 && this.tagName.toLowerCase() === 'img' && this.src !== '' ) {
+		add: function (handleObj) {
+			if (this.tagName.toLowerCase() === 'img' && this.src) {
 				// Image is already complete, fire the hollaback (fixes browser issues were cached
 				// images isn't triggering the load event)
-				if ( this.complete || this.readyState === 4 ) {
-					hollaback.handler.apply(this);
+				if (this.complete || this.readyState === 'complete') {
+					// manually dispatch load event *only* on this handler
+					// (existing handlers will have received load event normally)
+					var event = new $.Event('load');
+					event.currentTarget = event.target = this;
+					event.data = handleObj.data;
+					handleObj.handler.call(this, event);
 				}
 
 				// Check if data URI images is supported, fire 'error' event if not
-				else if ( this.readyState === 'uninitialized' && this.src.indexOf('data:') === 0 ) {
+				else if (this.readyState === 'uninitialized' && this.src.indexOf('data:') === 0) {
 					$(this).trigger('error');
-				}
-				
-				else {
-					$(this).bind('load', hollaback.handler);
 				}
 			}
 		}
